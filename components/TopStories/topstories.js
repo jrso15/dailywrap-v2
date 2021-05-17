313e271376635a89a10635de0eaaaa37261e6118
jrso15 <janilleolegario@gmail.com>
2021-05-18 01:34:20 +0800

    top stories

--- components/TopStories/topstories.js
+++ components/TopStories/topstories.js
@@ -1,45 +1,52 @@
-import { useState } from "react";
-
 import styles from "./topstories.module.scss";
 import utilStyles from "../../styles/utils.module.scss";
+import Image from "next/image";
 
-const TopStories = () => {
-  const [shouldShow, setShouldShow] = useState(false);
-  const [selectedStories, setSelectedStories] = useState([]);
-  const [topStories, setTopStories] = useState([
-    { _id: "5d7c7653427efd44002ertr0e", name: "sample1", url: "" },
-    { _id: "5d7c7653427efd44002egerr0r", name: "sample2", url: "" },
-    { _id: "5d7c7653427efd440020re0t", name: "sample3", url: "" },
-    { _id: "5d7c7653427efd4400gt", name: "sample4", url: "" },
-    { _id: "5d7c7653427efd440rrtrb020re0t", name: "sample5", url: "" },
-    { _id: "5d7c7653427efd440020rugge0t", name: "sample6", url: "" },
-    { _id: "5d7c7653427efd440ss020gfgre0t", name: "sample7", url: "" },
-    { _id: "5d7c7653427efd440020ggreg0t", name: "sample8", url: "" },
-    { _id: "5d7c7653427efd440020re0t", name: "sample9", url: "" },
-    { _id: "5d7c7653427efd440020reg0t", name: "sample10", url: "" },
-  ]);
-
-  const handleChanged = (event) => {
-    const isChecked = event.target.checked;
-    const id = event.target.getAttribute("data-id");
-    const stories = [...selectedStories];
-    const story = topStories.find((x) => x._id === id);
-
-    if (isChecked) {
-      stories.push(story);
-    } else {
-      stories.splice(stories.indexOf(story), 1);
-    }
-
-    setSelectedStories(stories);
-  };
-
+const TopStories = ({
+  stories,
+  selectedStories,
+  showSelectionBox,
+  onClickGetTopStories,
+  onCheckStory,
+  onClickNext,
+}) => {
   const showTopStories = () => {
-    return topStories.map((c, i) => (
+    console.log(stories);
+
+    if (stories === undefined || stories.items === undefined) return;
+
+    return stories.items.map((story, i) => (
       <label key={i}>
         <div className={styles.topStoriesList}>
-          <input onChange={handleChanged} data-id={c._id} type="checkbox" />
-          {c.name}
+          <input
+            onChange={onCheckStory}
+            data-title={story.title}
+            type="checkbox"
+          />
+          <div className={styles.storiesContainer}>
+            <div className={styles.storyContent}>
+              <Image
+                src={story["og-image"]}
+                alt={story.description}
+                width={300}
+                height={200}
+                className={styles.image}
+              />
+            </div>
+            <div className={styles.storyContent}>
+              <ul>
+                <li>
+                  <span>Title</span>: {story.title}
+                </li>
+                <li>
+                  <span>URL</span>: <a href={story.url}>{story.url}</a>
+                </li>
+                <li>
+                  <span>Time</span>: {story.timestamp}
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </label>
     ));
@@ -49,17 +56,14 @@ const TopStories = () => {
     <section className={styles.dailyWrapContainer}>
       <div className={styles.topStoriesHeader}>
         <h4 className={styles.title}>
-          {" "}
-          Hello editor want to know the top stories for today?{" "}
+          Hello editor want to know the top stories for today?
         </h4>
-        <button
-          className={utilStyles.btnSubmit}
-          onClick={() => setShouldShow(!shouldShow)}
-        >
+        <button className={utilStyles.btnSubmit} onClick={onClickGetTopStories}>
           Top Stories
         </button>
       </div>
-      {shouldShow ? (
+
+      {showSelectionBox ? (
         <div className={styles.topStoriesContainer}>
           <p>Please select 10 top stories:</p>
           <div className={styles.topStories}>
@@ -74,16 +78,28 @@ const TopStories = () => {
                 <h3 className={styles.titleHeader}>Selected top stories</h3>
               </div>
               <div className={styles.topStoriesBody}>
-                {selectedStories.map((item) => {
+                {selectedStories.map((item, i) => {
                   return (
-                    <div className={styles.topStoriesList} key={item._id}>
-                      {item.name}
+                    <div className={styles.topStoriesList} key={i}>
+                      <Image
+                        src={item["og-image"]}
+                        alt="Picture of the author"
+                        width={300}
+                        height={200}
+                        className={styles.image}
+                      />
+                      <a className={styles.selected} href={item.url}>
+                        {item.title}
+                      </a>
                     </div>
                   );
                 })}
               </div>
             </div>
           </div>
+          <button className={utilStyles.btnSubmit} onClick={onClickNext}>
+            Next
+          </button>
         </div>
       ) : null}
     </section>
