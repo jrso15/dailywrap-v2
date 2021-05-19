2d62af864ab03140feaffdd0b8f47ec9db161fa5
jrso15 <janilleolegario@gmail.com>
2021-05-20 01:36:22 +0800

    firebase

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAvbDfl6FBEU1kG-h-8x5zKZF9n_h8fmQk",
+  authDomain: "dailywrap-ctx.firebaseapp.com",
+  databaseURL: "https://dailywrap-ctx.firebaseio.com",
+  projectId: "dailywrap-ctx",
+  storageBucket: "dailywrap-ctx.appspot.com",
+  messagingSenderId: "544373503649",
+  appId: "1:544373503649:web:38dc83edc3749d29fee2e0",
+  measurementId: "G-3Q71XJCR30",
+};
