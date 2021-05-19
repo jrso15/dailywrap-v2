b0324f2c801f75b968cd16dae433d747a89229b2
jrso15 <janilleolegario@gmail.com>
2021-05-20 01:35:13 +0800

    add container to text editor

--- components/TextEditor/TextEditor.js
+++ components/TextEditor/TextEditor.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Editor, { buttonList } from "suneditor-react";
 import "suneditor/dist/css/suneditor.min.css";
+import styles from "./TextEditor.module.scss";
+import utilStyles from "../../styles/utils.module.scss";
 
 const TextEditor = ({ stories }) => {
-  console.log(stories);
   return (
-    <Editor
-      showToolbar={true}
-      enableToolbar={true}
-      setOptions={{
-        height: 500,
-      }}
-      setContents={stories}
-    />
+    <div className={styles.editorContainer}>
+      <p>Please input body text here:</p>
+      <Editor
+        showToolbar={true}
+        enableToolbar={true}
+        setOptions={{
+          height: 800,
+        }}
+        setContents={stories}
+      />
+      <div className={styles.btnWrapper}>
+        <button className={utilStyles.btnSubmit} onClick={null}>
+          Submit
+        </button>
+      </div>
+    </div>
   );
 };
 
