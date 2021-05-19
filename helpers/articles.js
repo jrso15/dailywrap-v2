4f8cb69d645c89df1b2fd7b287bbdb95d87a1a4c
jrso15 <janilleolegario@gmail.com>
2021-05-20 01:36:38 +0800

    new article.js

---
+++ helpers/articles.js
@@ -0,0 +1,32 @@
+import "firebase/firestore";
+
+export const getMetadata = async (selectedStories) => {
+  try {
+    const lhBaseApi = `https://api.rappler.com/article/posts`;
+
+    const metadata = await Promise.all(
+      selectedStories.map(async (selected) => {
+        const slug = selected.url.split("/").pop();
+        const apiUrl = `${lhBaseApi}/${slug}`;
+        const jsonData = await fetch(`${apiUrl}`, {
+          headers: { "Content-Type": "application/json" },
+        });
+        const articleData = await jsonData.json();
+
+        return {
+          href: articleData.href,
+          primaryTopic: articleData.primaryTopic[0].name,
+          title: articleData.title,
+          subhead: articleData.excerpt,
+          featuredImage: articleData.featuredImage.url,
+          thumbnailImage: articleData.thumbnailImages.url,
+        };
+      })
+    );
+    console.log(metadata);
+    return metadata;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
