8a7684093fb87f8813e9fbbc423414f3cc45cbba
jrso15 <janilleolegario@gmail.com>
2021-05-20 01:36:55 +0800

    firebase config

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,15 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/Layout/layout";
 import ParentComponent from "../components/TopStories/parentComponent";
 
+import firebase from "firebase/app";
+import firebaseConfig from "../config/firebase.js";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
 const Home = () => {
   return (
     <Layout home>
