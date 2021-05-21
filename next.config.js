32802501bae11b9004df0436c174963248f39de4
jrso15 <janilleolegario@gmail.com>
2021-05-21 13:44:30 +0800

    next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["rappler.com", "assets2.rappler.com", "i.ytimg.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
+  },
 };
