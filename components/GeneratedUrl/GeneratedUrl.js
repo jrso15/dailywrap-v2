cb87f463d37d7a29b495b434d67869440976607a
jrso15 <janilleolegario@gmail.com>
2021-05-21 19:36:55 +0800

    generated url component

--- components/GeneratedUrl/GeneratedUrl.js
+++ components/GeneratedUrl/GeneratedUrl.js
@@ -1,6 +1,11 @@
-const GeneratedUrl = () => {
-  console.log("URL");
-  return <p>Okay! We're all set! To generate html please go to this link:</p>;
+const GeneratedUrl = ({ id }) => {
+  return (
+    <p>
+      Okay! We're all set! To generate html please go to this link:
+      https://us-central1-rapplerinternal.cloudfunctions.net/dailywrap-dev-webhookmc?threads=$
+      {id}
+    </p>
+  );
 };
 
 export default GeneratedUrl;
