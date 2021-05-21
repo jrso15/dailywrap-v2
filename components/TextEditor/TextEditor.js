d14c0ef5b5d6b50a7e67bcde7ce0b5489f509903
jrso15 <janilleolegario@gmail.com>
2021-05-21 19:37:39 +0800

    fix text editor get contents and styles

--- components/TextEditor/TextEditor.js
+++ components/TextEditor/TextEditor.js
@@ -1,24 +1,60 @@
-import React from "react";
-import Editor, { buttonList } from "suneditor-react";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
 import "suneditor/dist/css/suneditor.min.css";
 import styles from "./TextEditor.module.scss";
 import utilStyles from "../../styles/utils.module.scss";
 
-const TextEditor = ({ stories, onClickSubmit }) => {
-  console.log(onClickSubmit);
+const SunEditor = dynamic(() => import("suneditor-react"), {
+  ssr: false,
+});
+
+const TextEditor = ({ onClickSubmit }) => {
+  const [text, setText] = useState("");
+
+  const handleChange = (content) => {
+    setText(content);
+  };
+
+  const onClickPreview = () => {
+    console.log("test");
+    const newWindow = window.open(
+      "/preview-template",
+      "_blank",
+      "noopener,noreferrer"
+    );
+    if (newWindow) newWindow.opener = null;
+  };
+
   return (
     <div className={styles.editorContainer}>
-      <p>Please input body text here:</p>
-      <Editor
+      <div className={styles.editorHeader}>
+        <p>Please input body text here:</p>
+        <a
+          className={styles.previewLink}
+          target="_blank"
+          href="/preview-template"
+          rel="noopener noreferrer"
+        >
+          Preview
+        </a>
+      </div>
+
+      <SunEditor
         showToolbar={true}
         enableToolbar={true}
         setOptions={{
           height: 500,
         }}
-        setContents={stories}
+        onChange={handleChange}
       />
+
       <div className={styles.btnWrapper}>
-        <button className={utilStyles.btnSubmit} onClick={onClickSubmit}>
+        <button
+          className={utilStyles.btnSubmit}
+          onClick={(e) => {
+            onClickSubmit(e, text);
+          }}
+        >
           Submit
         </button>
       </div>
