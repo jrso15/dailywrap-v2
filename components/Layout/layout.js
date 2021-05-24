aad649aa544d334929d49f8fefab698f90ffea4c
jrso15 <janilleolegario@gmail.com>
2021-05-24 15:15:01 +0800

    change icon and meta property

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -6,17 +6,15 @@ import utilStyles from "../../styles/utils.module.scss";
 const name = "DAILYWRAP";
 export const siteTitle = "Daily Wrap V2";
 
-const Layout = ({ children, home }) => {
+const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/logo.png" />
         <meta name="description" content="Daily Wrap" />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`/images/logo.png/${encodeURI(siteTitle)}.png`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
