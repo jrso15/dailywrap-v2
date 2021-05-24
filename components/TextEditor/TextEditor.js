96bbf8374bf588213217a8586b07b2c17a0e00f2
jrso15 <janilleolegario@gmail.com>
2021-05-24 15:15:21 +0800

    fixed handle preview

--- components/TextEditor/TextEditor.js
+++ components/TextEditor/TextEditor.js
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import "suneditor/dist/css/suneditor.min.css";
 import styles from "./TextEditor.module.scss";
 import utilStyles from "../../styles/utils.module.scss";
+import { getMetadata } from "../../helpers/articles";
 
 const SunEditor = dynamic(() => import("suneditor-react"), {
   ssr: false,
 });
 
-const TextEditor = ({ onClickSubmit }) => {
+const TextEditor = ({ selectedStories, onClickSubmit }) => {
   const [text, setText] = useState("");
+  const [storiesDb, setStoriesDb] = useState([]);
+
+  useEffect(() => {
+    console.log(storiesDb);
+    localStorage.setItem("stories", storiesDb);
+  }, [storiesDb]);
 
   const handleChange = (content) => {
     setText(content);
   };
 
-  const onClickPreview = () => {
-    console.log("test");
-    const newWindow = window.open(
-      "/preview-template",
-      "_blank",
-      "noopener,noreferrer"
-    );
-    if (newWindow) newWindow.opener = null;
+  const handlePreview = async () => {
+    const stories = await getMetadata(selectedStories);
+    const data = { stories, text };
+    setStoriesDb(JSON.stringify(data));
+    window.open("/preview", "_blank", "noopener noreferrer");
   };
 
   return (
     <div className={styles.editorContainer}>
       <div className={styles.editorHeader}>
         <p>Please input body text here:</p>
-        <a
-          className={styles.previewLink}
-          target="_blank"
-          href="/preview-template"
-          rel="noopener noreferrer"
-        >
+
+        <button className={styles.previewLink} onClick={handlePreview}>
           Preview
-        </a>
+        </button>
       </div>
 
       <SunEditor
